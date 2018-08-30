0d87c7889dacaf4785320259e976fa6b94fe78e1
puerco-potter <albiach92@hotmail.com>
2018-08-30 18:27:54 -0300

    inicial

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>100Monos</title>
+        <link rel='icon' type='image/png' href="images/monkey.png" />
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="AnimateIt/css/animations.css">
+        <link rel="stylesheet" href="AnimateIt/css/animations-ie-fix.css">
+        <link rel="stylesheet" href="css/estilo.css">
+        
+    </head>
+    <body>
+    	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="#Nosotros">100Monos</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#Publicitarias">Paginas Publicitarias</a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#Institucionales">Paginas Institucionales</a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#Contacto">Paginas De Contacto</a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#Tiendas">Tiendas</a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#Administrativas">Sistemas Administrativos</a>
+		      </li>
+		    </ul>
+		    <ul class="navbar-nav float-right">
+		    	<li class="nav-item active bg-success">
+		        	<a class="nav-link" href="#Contactenos">Contactenos</a>
+		      	</li>
+		    </ul>
+		  </div>
+		</nav>
+		<div>
+			<br>
+			<br>
+		</div>
+        <div id="Nosotros" class="pantalla d-flex align-items-center parallax justify-content-center fondoNosotros animatedParent">
+        	<div class="contenido text-light animated bounceIn delay-500">
+        		<h1>100Monos</h1>
+        		<h3>Somos un grupo de desarrollo y programación con sede en Resistencia, Chaco, Argentina.<br>
+        		 	Enfocado a brindar soluciones web y en sistemas de administracion, para empresas, emprendedores,<br>
+        			instituciones y particulares, a medida y ajustado a sus necesidades específicas.</h3>
+        	</div>
+        </div>
+        <div class="bg-light">
+        	<br>
+        </div>
+        <div id="Publicitarias" class="pantalla d-flex align-items-center parallax justify-content-center fondoPublicitarias animatedParent">
+        	<div class="contenido text-light animated flipInX delay-500">
+	        	<h1 class="titulo">Paginas Publicitarias</h1>
+				<h3>Permita que su cliente lo encuentre a usted cuando busca su producto o servicio.<br>
+				Hoy en día la gente busca en google antes de ir a cualquier lugar.<br>
+				Teniendo su propia pagina web usted empieza a aparecer en su radar.</h3>
+        	</div>
+        </div>
+        <div class="bg-light">
+        	<br>
+        </div>
+        <div id="Institucionales" class="pantalla d-flex align-items-center parallax justify-content-center fondoInstitucionales animatedParent">
+        	<div class="contenido text-light animated fadeInRightShort delay-900">
+	        	<h1>Paginas Institucionales</h1>
+	        	<h3>Exponga al publico su mision, su vision y su posición.<br>
+	        	De a conocer el por que y para que de su organización.<br>
+	        	En la era de la informacion el publico desconfia de lo que no es transparente.</h3>
+        	</div>
+        </div>
+        <div class="bg-light">
+        	<br>
+        </div>
+        <div id="Contacto" class="pantalla d-flex align-items-center parallax justify-content-center fondoContacto animatedParent">
+        	<div class="contenido text-light animated fadeInUpShort delay-900">
+	        	<h1>Paginas De Contacto</h1>
+	        	<h3>El publico quiere hablar con usted directamente, para expresar sus opiniones y dudas.<br>
+	        	Consiga la opinion del publico de primera mano.</h3>
+        	</div>
+        </div>
+        <div class="bg-light">
+        	<br>
+        </div>
+        <div id="Tiendas" class="pantalla d-flex align-items-center parallax justify-content-center fondoTiendas animatedParent">
+        	<div class="contenido text-light animated growIn delay-500">
+	        	<h1>Tiendas y Servicios</h1>
+	        	<h3>Que el publico tenga acceso a su producto o servicio directamente.<br>
+	        	Elimine intermediarios y cree un camino directo desde su organizacion a sus hogares.</h3>
+        	</div>
+        </div>
+        <div class="bg-light">
+        	<br>
+        </div>
+        <div id="Administrativas" class="pantalla d-flex align-items-center parallax justify-content-center fondoAdministrativas animatedParent">
+        	<div class="contenido text-light animated fadeInRightShort delay-900 text-light">
+	        	<h1>Sistemas Administrativos</h1>
+				    <h3>Mantenga organizada y practica la información de sus clientes, <br>empleados, proveedores, stock y todo lo que pueda necesitar.</h3>
+        	</div>
+        </div>
+        <div class="bg-light">
+        	<br>
+        </div>
+        <div id="Contactenos" class="pantalla d-flex align-items-center parallax justify-content-center fondoContactenos animatedParent">
+        	<div class="contenido text-light animated pulse delay-900 text-light">
+	        	<h1>Contactenos</h1>
+				    <h3>100MonosDesarrollo@gmail.com</h3>
+        	</div>
+        </div>
+
+       	<div class="d-flex justify-content-center text-light bg-dark">
+       		<div>
+	       		<div class="d-flex justify-content-center">
+	       			<div>Pagina creada por Grupo de Desarrollo <a href="http://www.100monos.com">100monos</a></div>
+	       		</div>
+	       		<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+	       	</div>
+       	</div>
+    </body>
+    <script src="AnimateIt/js/css3-animate-it.js"></script>
+</html>
