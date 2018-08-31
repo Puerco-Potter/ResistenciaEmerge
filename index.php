ddb697ec4694d5b57e1287943e14bd32794d6874
puerco-potter <albiach92@hotmail.com>
2018-08-30 23:58:00 -0300

    diseño

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>100Monos</title>
-        <link rel='icon' type='image/png' href="images/monkey.png" />
+        <title>Resistencia Emerge</title>
+        <link rel='icon' type='image/png' href="images/logo.png" />
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -17,8 +17,8 @@
         
     </head>
     <body>
-    	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-		  <a class="navbar-brand" href="#Nosotros">100Monos</a>
+    	<nav class="navbar navbar-dark fondoBarra fixed-top navbar-expand-lg navbar-light bg-light">
+		  <a style="width:7%;" class="navbar-brand" href="#Nosotros"><img style="width:100%;" src="images/Blanco.png"></a>
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
@@ -26,24 +26,24 @@
 		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		    <ul class="navbar-nav mr-auto">
 		      <li class="nav-item active">
-		        <a class="nav-link" href="#Publicitarias">Paginas Publicitarias</a>
+		        <a class="nav-link" href="#Publicitarias">Objetivos</a>
 		      </li>
 		      <li class="nav-item active">
-		        <a class="nav-link" href="#Institucionales">Paginas Institucionales</a>
+		        <a class="nav-link" href="#Institucionales">Contactos e Información</a>
 		      </li>
 		      <li class="nav-item active">
-		        <a class="nav-link" href="#Contacto">Paginas De Contacto</a>
+		        <a class="nav-link" href="#Contacto">Programa</a>
 		      </li>
 		      <li class="nav-item active">
-		        <a class="nav-link" href="#Tiendas">Tiendas</a>
+		        <a class="nav-link" href="#Tiendas">Expositores</a>
 		      </li>
 		      <li class="nav-item active">
-		        <a class="nav-link" href="#Administrativas">Sistemas Administrativos</a>
+		        <a class="nav-link" href="#Administrativas">Hoteles</a>
 		      </li>
 		    </ul>
 		    <ul class="navbar-nav float-right">
-		    	<li class="nav-item active bg-success">
-		        	<a class="nav-link" href="#Contactenos">Contactenos</a>
+		    	<li class="nav-item active fondoboton">
+		        	<a class="nav-link" href="#Contactenos">Inscribirse a la Jornada</a>
 		      	</li>
 		    </ul>
 		  </div>
@@ -52,72 +52,41 @@
 			<br>
 			<br>
 		</div>
-        <div id="Nosotros" class="pantalla d-flex align-items-center parallax justify-content-center fondoNosotros animatedParent">
-        	<div class="contenido text-light animated bounceIn delay-500">
-        		<h1>100Monos</h1>
-        		<h3>Somos un grupo de desarrollo y programación con sede en Resistencia, Chaco, Argentina.<br>
-        		 	Enfocado a brindar soluciones web y en sistemas de administracion, para empresas, emprendedores,<br>
-        			instituciones y particulares, a medida y ajustado a sus necesidades específicas.</h3>
+        <div id="Nosotros" class="pantalla d-flex align-items-center justify-content-center animatedParent fondoHome">
+        	<div class="contenido text-light text-center">
+        		<img style="width:60%;" src="images/LogoYNombre.png">
+        		<h2>PRIMERAS JORNADAS REGIONALES DE SENSIBILIZACIÓN TURÍSTICA MUNICIPAL</h2>
+                <h1><x class="negrita">27</x> y <x class="negrita">28</x> de <x class="negrita">Setiembre</x> de <x class="negrita">2018</x></h1>
         	</div>
         </div>
-        <div class="bg-light">
-        	<br>
-        </div>
-        <div id="Publicitarias" class="pantalla d-flex align-items-center parallax justify-content-center fondoPublicitarias animatedParent">
+        <div id="Publicitarias" class="foto d-flex align-items-center parallax justify-content-center fondoPublicitarias animatedParent">
         	<div class="contenido text-light animated flipInX delay-500">
-	        	<h1 class="titulo">Paginas Publicitarias</h1>
-				<h3>Permita que su cliente lo encuentre a usted cuando busca su producto o servicio.<br>
-				Hoy en día la gente busca en google antes de ir a cualquier lugar.<br>
-				Teniendo su propia pagina web usted empieza a aparecer en su radar.</h3>
         	</div>
         </div>
-        <div class="bg-light">
-        	<br>
-        </div>
-        <div id="Institucionales" class="pantalla d-flex align-items-center parallax justify-content-center fondoInstitucionales animatedParent">
-        	<div class="contenido text-light animated fadeInRightShort delay-900">
-	        	<h1>Paginas Institucionales</h1>
-	        	<h3>Exponga al publico su mision, su vision y su posición.<br>
-	        	De a conocer el por que y para que de su organización.<br>
-	        	En la era de la informacion el publico desconfia de lo que no es transparente.</h3>
+        <div id="Institucionales" class="pantalla d-flex align-items-center justify-content-center animatedParent fondoHome">
+        	<div class="contenido text-light text-center animated fadeInUpShort delay-1300">
+	        	<h1>OBJETIVOS:</h1>
+	        	<h3>Instalar a la comunidad en general, y al sector vinculado directa e indirectamente al Turismo Receptivo, la importancia potencial que genera el turismo desde el punto de vista social, cultural, productivo y laboral a través de diferentes ponencias que permitan una mejor interpretación de la misma, conociendo las experiencias, avances y resultados de diferentes propuestas y programas, a escala de la Ciudad de Resistencia.</h3>
         	</div>
         </div>
-        <div class="bg-light">
-        	<br>
-        </div>
-        <div id="Contacto" class="pantalla d-flex align-items-center parallax justify-content-center fondoContacto animatedParent">
+        <div id="Contacto" class="foto d-flex align-items-center parallax justify-content-center fondoContacto animatedParent fondoHome">
         	<div class="contenido text-light animated fadeInUpShort delay-900">
-	        	<h1>Paginas De Contacto</h1>
-	        	<h3>El publico quiere hablar con usted directamente, para expresar sus opiniones y dudas.<br>
-	        	Consiga la opinion del publico de primera mano.</h3>
         	</div>
         </div>
-        <div class="bg-light">
-        	<br>
-        </div>
-        <div id="Tiendas" class="pantalla d-flex align-items-center parallax justify-content-center fondoTiendas animatedParent">
-        	<div class="contenido text-light animated growIn delay-500">
-	        	<h1>Tiendas y Servicios</h1>
-	        	<h3>Que el publico tenga acceso a su producto o servicio directamente.<br>
-	        	Elimine intermediarios y cree un camino directo desde su organizacion a sus hogares.</h3>
+        <div id="Tiendas" class="pantalla d-flex align-items-center justify-content-center animatedParent fondoHome">
+        	<div class="contenido text-light animated growIn delay-500 text-center">
+	        	<h1>Contactos e Información</h1>
+	        	<h3>Llame ya</h3>
         	</div>
         </div>
-        <div class="bg-light">
-        	<br>
-        </div>
-        <div id="Administrativas" class="pantalla d-flex align-items-center parallax justify-content-center fondoAdministrativas animatedParent">
+        <div id="Administrativas" class="foto d-flex align-items-center parallax justify-content-center fondoAdministrativas animatedParent">
         	<div class="contenido text-light animated fadeInRightShort delay-900 text-light">
-	        	<h1>Sistemas Administrativos</h1>
-				    <h3>Mantenga organizada y practica la información de sus clientes, <br>empleados, proveedores, stock y todo lo que pueda necesitar.</h3>
         	</div>
         </div>
-        <div class="bg-light">
-        	<br>
-        </div>
-        <div id="Contactenos" class="pantalla d-flex align-items-center parallax justify-content-center fondoContactenos animatedParent">
-        	<div class="contenido text-light animated pulse delay-900 text-light">
-	        	<h1>Contactenos</h1>
-				    <h3>100MonosDesarrollo@gmail.com</h3>
+        <div id="Contactenos" class="pantalla d-flex align-items-center justify-content-center animatedParent fondoHome">
+        	<div class="contenido text-light text-center animated flipInX delay-900">
+	        	<h1>INSCRIPCIÓN:</h1>
+				    <a class="btn btn-primary fondoboton" href="#"><h3 style="color: white">Incribirse a la Jornada</h3></a>
         	</div>
         </div>
 
@@ -126,7 +95,6 @@
 	       		<div class="d-flex justify-content-center">
 	       			<div>Pagina creada por Grupo de Desarrollo <a href="http://www.100monos.com">100monos</a></div>
 	       		</div>
-	       		<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 	       	</div>
        	</div>
     </body>
