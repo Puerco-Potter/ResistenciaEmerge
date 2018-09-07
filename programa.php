5b09c8110fb82071b3bafb94e49449b6b80078e1
wolverine20 <arandapablodamian@gmail.com>
2018-09-07 01:14:29 -0300

    comienzo de programa

--- programa.php
+++ programa.php
@@ -13,8 +13,336 @@
     	?>
 
         <div id="Contactenos" class="pantalla d-flex align-items-center justify-content-center animatedParent fondoHome">
-            <div class="contenido text-light text-center">
-                <h3>De momento esta seccion de la web no se encuentra disponible. Lo estara a la brevedad.</h3>
+            <div class="contenido text-light text-center " style="margin-top:10%;">
+            <div style="width:80%; margin-right:auto;margin-left:auto;">
+                <h2> “PRIMERAS JORNADAS REGIONALES DESENSIBILIZACION TURÍSTICA MUNICIPAL”</h2> 
+                <hr>
+                <p>
+                <div style="color:black;text-align:left;">
+                    <strong>Fechas:</strong> Jueves 27 y Viernes 28 de Setiembre de 2018
+                    <br>
+                    <strong>Lugar:</strong>  Centro de Convenciones del Domo del Centenario, (Avda de los Inmigrantes y Río Negro. Resistencia).
+                    <hr>
+                    <h4>Objetivos:</h4>
+                    <p>Instalar a la comunidad en general, y al sector vinculado directa e indirectamente al Turismo Receptivo, la importancia potencial que genera el turismo desde el    punto e vista social, cultural, productivo y laboral a través de diferentes ponencias que permitan una mejor interpretación de la misma, conociendo las experiencias,     avances y resultados de diferentes propuestas y programas, a escala de la Ciudad de Resistencia.</p>
+                </div>
+                    <h2>Programa</h2>
+                    <br>
+                    <table class="table table-striped" style="color:black;">
+                        <thead class="thead-dark">
+                            <tr>
+                            <th scope="col" >Jueves 27 de Septiembre</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    08,00 hs:
+                                    Recepción y Acreditaciones al público.
+
+                                    
+
+                                </td>
+                            
+                            </tr>
+                            <tr>
+                                <td>
+                                    10,00 hs:
+                                    Acto de Apertura:
+                                    •	Palabras del Presidente del Concejo Municipal de Resistencia                              MMO Gustavo Martínez.
+                                    •	Palabras del Intendente de la Ciudad de Resistencia                                      CPN Jorge M. Capitanich
+                                    •	Palabras del Gobernador de la Provincia del Chaco
+                                    Ing, Oscar Domingo Peppo.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        10,30 hs: Primera Ponencia, (30 minutos).
+                                        “Resistencia: Ciudad Emergente”: Arq. Luis Alejandro Gusberti ,(Resistencia)
+
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7">
+                                            •	Arquitecto egresado d la UNNE
+                                            •	Ex Presidente de la Sociedad de Arquitectos del Chaco, (SACH)
+                                            •	Ex Secretario de Prensa de FADEA, (Federación de Entidades de Arquitectos de la Argentina)
+                                            •	Titular del Grupo de Acceso al Siglo 21 ,(Estudio de Arquitectura y Planificación Regional para Turismo Receptivo) 
+                                            •	Trabajó para agencias soporte de Televisa SAdeCV (México)
+                                            •	Desde Julio 2018 a cargo del proyecto de Planificación de Turismo Receptivo del Concejo Municipal de Resistencia.
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        11,00 hs: Preguntas del Público, (10 minuto
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        11,30 hs; Segunda Ponencia, (30 minutos).
+                                        “El Compromiso del Sector Privado para con el Turismo Receptivo”:
+                                        Lic. Verónica Mazzaroli, (Resistencia)
+
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7">
+                                            •	Técnica en Administración de Empresas.
+                                            •	Empresaria local del rubro gastronómico
+                                            •	Actual Vicepresidente de la Asociación de Hoteles, Restaurantes, Confitería y Afines del Chaco.
+                                            •	Ex Subsecretaría de Turismo de la Municipalidad de Resistencia
+                                            •	Consejera a nivel Nacional de FEHGRA.
+                                            •	Participación de la XVII Feria Internacional de Turismo Termal ¨Termatalia¨ y asistencia al Simposio. España, (Ourense).
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        11,00 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        11,30 hs: Pausa Para Café.
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        12,15 hs; Tercera Ponencia, (30 minutos).
+                                        “Resistencia : ¿Ciudad Turística? ” :Lic. Ángela Ayala, (Ciudad de Buenos Aires).
+
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7">
+                                            •	Egresada de Ciencias de la Comunicación UNNE y UNLP
+                                            •	Cursos de Post grado: Alta Gerencia Pública (INAP.) 
+                                            •	Educación de Adultos (UBA) Política y Desarrollo del Turismo y Turismo Rural, Universidad Pantheon Sorbonne IV, Paris, Francia. Organización de Hoteles. Universidad de Alcalá de Henares- España e Int. Internacional de TURISMO .MARBELLA .España 
+                                            •	se  desempeño en la Secretaria de Turismo de la Nación en el Dpto. de Recursos Humanos  y desde 1993  como Directora Ejecutiva de Relaciones Institucionales y  actualmente  y en el Ministerio de Turismo de la Nación en la   Calidad y Formación para el Turismo por  Concurso Público  
+                                            •	Creadora del Programa Federal de Formación y Capacitación en turismo para la Secretaría y luego Ministerio de Turismo de la Nación, mas de 3 mil acciones realizadas..
+
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        12,45 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        13,00 hs : Intervalo . Corte para café-almuerzo.
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                    14,15 hs: Cuarta Ponencia, ( 30 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        “Turismo y Accesibilidad” ; Cinthia Vega, (Resistencia).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        14,45 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        15,00 hs: Quinta Ponencia, ( 30 minutos).
+                                        “La accesibilidad y el Turismo": Prof. Alejandro Piccione, (Ciudad de Buenos Aires).-
+
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7">
+                                            •	Docente terciario
+                                            •	Guía Nacional de Turismo y Guía intérprete medioambiental de APN
+                                            •	Socio gerente y fundador del complejo de cabañas accesibles Cultura Serrana-Cordoba
+                                            •	Coordinador general de promoción turismo interno en las campañas “Viaja por Tu País”, “Argentina es tu Mundo, Date Una vuelta” y “Elijo Viajar” del Ministerio de Turismo de la Nación 
+                                            •	Co-fundador y socio gerente de Consultora Turística 50 kilómetros
+                                            •	Co-fundador y director de la Red de Turismo Accesible de la Argentina
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        15,30 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        16,00 hs: Pausa Para Café.
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        16,30 hs: Sexta Ponencia, (30 minutos).
+                                        “Turismo y Cooperativismo”: Representante de Buenos Aires sugerido por Cjal Alicia Frias,
+
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        17,00 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        17,30 hs: Septima Ponencia, (30 minutos).
+                                        “Tema a Definir” : Lic, 
+
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        18,00 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        18,30 hs: Cierra Jornadas del día Jueves 27
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        14,45 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        14,45 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="titulo">
+                                        14,45 hs: Preguntas del Público, (10 minutos).
+                                    <div>
+                                    <div class="row">
+                                        <div class="col-5"></div>
+                                        <div class="col-7"></div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
             </div>
         </div>
        	<?php
