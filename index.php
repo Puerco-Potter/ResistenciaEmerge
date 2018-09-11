c1162c03f24afe12048d00f5bb5c6c16b87c0e9c
puerco-potter <albiach92@hotmail.com>
2018-09-11 07:10:36 -0300

    cambios chicos

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 			<br>
 			<br>
 		</div>
-        <div id="Nosotros" class="pantalla d-flex align-items-center justify-content-center animatedParent fondoHome">
+        <div id="Nosotros" class="pantalla d-flex align-items-center justify-content-center fondoHome">
         	<div class="contenido text-light text-center">
         		<img style="width:30%;" src="images/LogoYNombre.png">
                 <br>
@@ -25,8 +25,8 @@
                 <h1><x class="negrita">27</x> y <x class="negrita">28</x> de <x class="negrita">Setiembre</x> de <x class="negrita">2018</x></h1>
         	</div>
         </div>
-        <div id="Publicitarias" class="foto d-flex align-items-center parallax justify-content-center fondoPublicitarias animatedParent">
-        	<div class="contenido text-light animated flipInX delay-500">
+        <div id="Publicitarias" class="foto align-items-center parallax justify-content-center fondoPublicitarias">
+        	<div class="contenido text-light">
         	</div>
         </div>
         <div id="Objetivos" class="pantalla d-flex align-items-center justify-content-center animatedParent fondoHome">
@@ -35,49 +35,53 @@
 	        	<h3>Instalar a la comunidad en general, y al sector vinculado directa e indirectamente al Turismo Receptivo, la importancia potencial que genera el turismo desde el punto de vista social, cultural, productivo y laboral a través de diferentes ponencias que permitan una mejor interpretación de la misma, conociendo las experiencias, avances y resultados de diferentes propuestas y programas, a escala de la Ciudad de Resistencia.</h3>
         	</div>
         </div>
-        <div id="Contacto" class="foto d-flex align-items-center parallax justify-content-center fondoContacto animatedParent fondoHome">
+        <div id="Contacto" class="foto align-items-center parallax justify-content-center fondoContacto animatedParent fondoHome">
         	<div class="contenido text-light animated fadeInUpShort delay-900">
         	</div>
         </div>
         <div id="Informacion" class="pantalla d-flex align-items-center justify-content-center animatedParent fondoHome">
         	<div class="contenido text-light animated growIn delay-500 text-center">
 	        	<h1>Contactos e Información</h1>
-	        	
+	        	<div class="container">
                 <div class="text-center">
                     
                         <br><h3>Telefonos de contacto:</h3>
                 </div>
+                
 	        	<div class="row">
-	        			<div class="col-sm-6"><h3>Telefono 1:</h3><x style="width: 20px"></x></div>
-	        			<div class="col-sm-6"><h1><x class="negrita">+54 9 362 4458290</x></h1></div>
+	        			<div class="col-md-6"><h3>Telefono 1:</h3><x style="width: 20px"></x></div>
+	        			<div class="col-md-6"><h1><x class="negrita">+54 9 362 4458290</x></h1></div>
 	        	</div>
+                
 	        	<div class="row">
 	        		
-	        			<div class="col-sm-6"><h3>Telefono 2:</h3><x style="width: 20px"></x></div>
-	        			<div class="col-sm-6"><h1><x class="negrita">+54 9 362 4458360</x></h1></div>
+	        			<div class="col-md-6"><h3>Telefono 2:</h3><x style="width: 20px"></x></div>
+	        			<div class="col-md-6"><h1><x class="negrita">+54 9 362 4458360</x></h1></div>
 	        	</div>
+                
                 <div class="text-center">
                     
                         <br><h3>E-mails de contacto de los organizadores:</h3>
                 </div>
                 <div class="row">
                     
-                        <div class="col-sm-6 text-left"><h3>Concejal Alicia Frías:</h3><x style="width: 40px"></x></div>
-                        <div class="col-sm-6"><h1><x class="negrita">contactosaliciafrias@gmail.com</x></h1></div>
+                        <div class="col-md-6 text-left"><h3>Concejal Alicia Frías:</h3><x style="width: 40px"></x></div>
+                        <div class="col-md-6"><h1><x class="negrita">contactosaliciafrias@gmail.com</x></h1></div>
                 </div>
                 <div class="row">
                     
-                        <div class="col-sm-6 text-left"><h3>Arquitecto Luis Gusberti:</h3><x style="width: 20px"></x></div>
-                        <div class="col-sm-6"><h1><x class="negrita">arqgusberti@gmail.com</x></h1></div>
+                        <div class="col-md-6 text-left"><h3>Arquitecto Luis Gusberti:</h3><x style="width: 20px"></x></div>
+                        <div class="col-md-6"><h1><x class="negrita">arqgusberti@gmail.com</x></h1></div>
+                </div>
                 </div>
 	        	
         	</div>
         </div>
-        <div id="Administrativas" class="foto d-flex align-items-center parallax justify-content-center fondoAdministrativas animatedParent">
-        	<div class="contenido text-light animated fadeInRightShort delay-900 text-light">
+        <div id="Administrativas" class="foto align-items-center parallax justify-content-center fondoAdministrativas">
+        	<div class="contenido text-light">
         	</div>
         </div>
-        <div id="Contactenos" class="pantalla text-center animatedParent fondoHome">
+        <div id="Contactenos" class="pantalla text-center fondoHome">
             <div style="height: 150px;"></div>
         	<div class="contenido text-light text-center">
                 <h1>INSCRIPCIÓN:</h1>
@@ -86,36 +90,38 @@
             <div style="height: 50px;"></div>
             <h3>Participan:</h3>
             <br>
+            <div class="container">
             <div class="row">
                 <div style="width: 5%"></div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-2"><img width="80%" src="images/isologos/A.png"></div>
-                <div class="col-sm-2"></div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-1"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-2 col-md-6 col-sm-12"><img width="80%" src="images/isologos/A.png"></div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-1"></div>
                 <div style="width: 5%"></div>
             </div>
             <div class="row">
                 <div style="width: 5%"></div>
-                <div class="col-sm-1"></div>
-                <div class="col-sm-1"><img width="100%" src="images/isologos/S.png"></div>
-                <div class="col-sm-2"><img width="100%" src="images/isologos/E.png"></div>
-                <div class="col-sm-2"><img width="100%" src="images/isologos/P.png"></div>
-                <div class="col-sm-1"><img width="100%" src="images/isologos/G.png"></div>
-                <div class="col-sm-1"><img width="100%" src="images/isologos/H.png"></div>
-                <div class="col-sm-2"><img width="100%" src="images/isologos/D.jpg"></div>
-                <div class="col-sm-1"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-1 col-md-3 col-sm-6"><img width="100%" src="images/isologos/S.png"></div>
+                <div class="col-lg-2 col-md-6 col-sm-12"><img width="100%" src="images/isologos/E.png"></div>
+                <div class="col-lg-2 col-md-6 col-sm-12"><img width="100%" src="images/isologos/P.png"></div>
+                <div class="col-lg-1 col-md-3 col-sm-6"><img width="100%" src="images/isologos/G.png"></div>
+                <div class="col-lg-1 col-md-3 col-sm-6"><img width="100%" src="images/isologos/H.png"></div>
+                <div class="col-lg-2 col-md-6 col-sm-12"><img width="100%" src="images/isologos/D.jpg"></div>
+                <div class="col-lg-1"></div>
                 <div style="width: 5%"></div>
+            </div>
             </div>
             <div style="height: 20px;"></div>
         </div>
-        <div id="Contacto" class="foto d-flex align-items-center parallax justify-content-center fondoFinal animatedParent fondoHome">
-            <div class="contenido text-light animated fadeInUpShort delay-900">
+        <div id="Contacto" class="foto align-items-center parallax justify-content-center fondoFinal fondoHome">
+            <div class="contenido text-light">
             </div>
         </div>
 
