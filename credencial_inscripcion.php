ff6bdf76332f9c79fafef8c9782185330e4c0e30
Gerardo Budzovsky <gerardo.dav.bud@gmail.com>
2018-09-18 12:18:48 -0300

    Gafete casi listo, falta cambiar la fuente del nombre y apellido.

--- credencial_inscripcion.php
+++ credencial_inscripcion.php
@@ -10,46 +10,46 @@ $inscripcion_id= $_GET['inscripcion_id'];
 class PDF extends FPDF
 {
 	// Cabecera de pagina
-	function Header()
-	{
+//	function Header()
+//	{
 	    // Logo
-	    $this->Image('images/LogoYNombre.png',10,8,50);
+	    //$this->Image('images/LogoYNombre.png',10,8,50);
 	    // Arial bold 15
-	    $this->SetFont('Arial','B',15);
+//	    $this->SetFont('Arial','B',15);
 	    // Movernos a la derecha
-	    $this->Cell(60);
+//	    $this->Cell(60);
 	    // Titulo
 	    //$this->Cell(80,10,'Resistencia Emerge',1,0,'C');
 	    // Salto de linea
 	    //$this->Ln(20);
-	}
+//	}
 
 	// Pie de pagina
-	function Footer()
-	{
+//	function Footer()
+//	{
 	    // Posicion: a 1,5 cm del final
-	    $this->SetY(-15);
+//	    $this->SetY(-15);
 	    // Arial italic 8
-	    $this->SetFont('Arial','I',8);
+//	    $this->SetFont('Arial','I',8);
 	    // Numero de pagina
-	    $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
-	}
+	    //$this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
+//	}
 }
 
 	// Creacion del objeto de la clase heredada
 	$pdf = new PDF();
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
-	$pdf->Cell(0,10,"",0,1);
-	$pdf->SetFont('Times','b',16);
 
 	$conn= conectar();
 	$filas= mysqli_query($conn, "SELECT * FROM inscripciones WHERE id=".$inscripcion_id);
 
-	$pdf->SetFont('Times','',11);
+	$pdf->Image('images/gafeteDeParticipantes.jpg',0,0,200,300,'JPG');
+	$pdf->SetFont('Times','',20);
 	$fila=mysqli_fetch_array($filas);
-	$pdf->Cell(0,10,"",0,1);
-	$pdf->Cell(0,10,"Apellido y Nombre: ". $fila['apellido'].", ".$fila['nombre'],0,1);
+	$pdf->Cell(0,160,"",0,1);
+	//$pdf->Cell(0,10,"Apellido y Nombre: ". $fila['apellido'].", ".$fila['nombre'],0,1);
+	$pdf->Cell(0,20,"    ".$fila['apellido'].", ".$fila['nombre'],0,1);
 	$pdf->Output();
 
 
